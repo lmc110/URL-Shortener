1a39e97cfc3b460121f592be26b8a43bd7e78e94
luism <luismcampos32@gmail.com>
2016-08-18 14:39:11 -0500

    Form created on index page

--- myurlshortener.py
+++ myurlshortener.py
@@ -1,11 +1,23 @@
 import flask
+import os
+import json
 
 app = flask.Flask(__name__)
+
+urlDict = {}
+
+if os.path.exists('bookmarks.json'):
+    urlDict = json.load(open('bookmarks.json', 'r', encoding='UTF8'))
 
 
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
+
+@app.route('/shorten', methods=['POST'])
+def shorten():
+    pass
 
 
 if __name__ == '__main__':
